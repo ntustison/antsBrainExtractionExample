bd8919d9752c26b8e1283ce4e0a7e94522dfc1f7
Nick Tustison <ntustison@gmail.com>
2015-05-11 16:49:09 -0700

    ENH:  Changed to a better slice.

--- antsBrainExtractionCommand.sh
+++ antsBrainExtractionCommand.sh
@@ -7,7 +7,7 @@ export ANTSPATH=/Users/ntustison/Pkg/ANTs/bin/bin/
 
 bash ${ANTSPATH}antsBrainExtraction.sh \
   -d 2 \
-  -a ${DATA_DIR}KKI2009-01-MPRAGE_slice150.nii.gz \
+  -a ${DATA_DIR}IXI002-Guys-0828-T1_slice90.nii.gz \
   -e ${TEMPLATE_DIR}T_template0_slice122.nii.gz \
   -m ${TEMPLATE_DIR}T_template0ProbabilityMask.nii.gz \
   -o ${OUT_DIR}example
